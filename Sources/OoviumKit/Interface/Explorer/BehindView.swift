a6528bb24436b9b133900cfb4dda373b93647305
Joe Charlier <github@aepryus.com>
2022-03-13 10:34:45 +0900

    make things public

--- Sources/OoviumKit/Interface/Explorer/BehindView.swift
+++ Sources/OoviumKit/Interface/Explorer/BehindView.swift
@@ -10,35 +10,38 @@ import Acheron
 import OoviumEngine
 import UIKit
 
-//class BehindView: UIView {
-//	let leftExplorer: AetherExplorer = AetherExplorer(aetherView: Oovium.aetherView)
-//	let rightExplorer: AetherExplorer = AetherExplorer(aetherView: Oovium.aetherView)
-//	let newTrapezoid: Trapezoid = Trapezoid(title: "New Aether".localized)
-//	let expandTrapezoid: Trapezoid = Trapezoid(title: "Manage".localized, flipped: true)
-//
-//	init() {
-//		super.init(frame: .zero)
-//
-//		backgroundColor = UIColor(red: 32/255, green: 34/255, blue: 36/255, alpha: 1)
-//
-//		addSubview(newTrapezoid)
-//		addSubview(leftExplorer)
-//		addSubview(expandTrapezoid)
-//
-//		newTrapezoid.addAction { [unowned self] in
-//			self.leftExplorer.space.newAether { (aether: Aether?) in
-//				guard let aether = aether else { return }
-//				self.leftExplorer.aetherView.swapToAether(space: self.leftExplorer.space, aether: aether)
-//				self.leftExplorer.aetherView.slideBack()
-//			}
-//		}
-//	}
-//	required init?(coder: NSCoder) { fatalError() }
-//
-//// UIView ==========================================================================================
-//	override func layoutSubviews() {
-//		leftExplorer.frame = CGRect(x: 5*s, y: 100*s, width: 355*s, height: Screen.height-Screen.safeBottom-110*s-50*s)
-//		newTrapezoid.frame = CGRect(x: leftExplorer.right - 172*s - 25*s, y: leftExplorer.top - 4*s-40*s, width: 172*s, height: 40*s)
-//		expandTrapezoid.frame = CGRect(x: leftExplorer.right - 172*s - 25*s, y: leftExplorer.bottom + 4*s, width: 172*s, height: 40*s)
-//	}
-//}
+public class BehindView: UIView {
+	let leftExplorer: AetherExplorer
+	let rightExplorer: AetherExplorer
+	let newTrapezoid: Trapezoid = Trapezoid(title: "New Aether".localized)
+	let expandTrapezoid: Trapezoid = Trapezoid(title: "Manage".localized, flipped: true)
+
+	public init(aetherView: AetherView) {
+		leftExplorer = AetherExplorer(aetherView: aetherView)
+		rightExplorer = AetherExplorer(aetherView: aetherView)
+
+		super.init(frame: .zero)
+
+		backgroundColor = UIColor(red: 32/255, green: 34/255, blue: 36/255, alpha: 1)
+
+		addSubview(newTrapezoid)
+		addSubview(leftExplorer)
+		addSubview(expandTrapezoid)
+
+		newTrapezoid.addAction { [unowned self] in
+			self.leftExplorer.space.newAether { (aether: Aether?) in
+				guard let aether = aether else { return }
+				self.leftExplorer.aetherView.swapToAether(space: self.leftExplorer.space, aether: aether)
+				self.leftExplorer.aetherView.slideBack()
+			}
+		}
+	}
+	required init?(coder: NSCoder) { fatalError() }
+
+// UIView ==========================================================================================
+	override public func layoutSubviews() {
+		leftExplorer.frame = CGRect(x: 5*s, y: 100*s, width: 355*s, height: Screen.height-Screen.safeBottom-110*s-50*s)
+		newTrapezoid.frame = CGRect(x: leftExplorer.right - 172*s - 25*s, y: leftExplorer.top - 4*s-40*s, width: 172*s, height: 40*s)
+		expandTrapezoid.frame = CGRect(x: leftExplorer.right - 172*s - 25*s, y: leftExplorer.bottom + 4*s, width: 172*s, height: 40*s)
+	}
+}
