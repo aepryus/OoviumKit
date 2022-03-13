560f8dc110007ec70e161bcf84c3c1983be088eb
Joe Charlier <github@aepryus.com>
2022-03-14 08:25:21 +0900

    moved leftExplorer

--- Sources/OoviumKit/Interface/Explorer/BehindView.swift
+++ Sources/OoviumKit/Interface/Explorer/BehindView.swift
@@ -41,7 +41,7 @@ public class BehindView: UIView {
 // UIView ==========================================================================================
 	override public func layoutSubviews() {
 		let y: CGFloat = 5*s + (Screen.mac ? Screen.safeTop : 0) + 44*s
-		leftExplorer.frame = CGRect(x: 5*s, y: y, width: 355*s, height: Screen.height - Screen.safeBottom - y - 44*s)
+		leftExplorer.frame = CGRect(x: 5*s, y: y, width: 355*s, height: Screen.height - Screen.safeBottom - y - 44*s - 5*s)
 		newTrapezoid.frame = CGRect(x: leftExplorer.right - 172*s - 25*s, y: leftExplorer.top - 4*s-40*s, width: 172*s, height: 40*s)
 		expandTrapezoid.frame = CGRect(x: leftExplorer.right - 172*s - 25*s, y: leftExplorer.bottom + 4*s, width: 172*s, height: 40*s)
 	}
