4c734e072ae789ae610109664963402131793f41
Joe Charlier <github@aepryus.com>
2024-09-08 21:13:59 +0900

    GridBub work

--- Sources/OoviumKit/Interface/Aether/Oovium/GridBub/GridController.swift
+++ Sources/OoviumKit/Interface/Aether/Oovium/GridBub/GridController.swift
@@ -31,68 +31,68 @@ class GridController: ChainViewKeyDelegate {
     }
     
     func addRow() {
-//        let cells: [Cell] = grid.addRow()
-//        gridBub.addRow(with: cells)
-//        grid.columns.filter({ $0.aggregate != .none }).forEach {
-//            $0.footerChain.tower.buildStream()
-//            $0.footerChain.tower.buildTask()
-//        }
-//        resizeEverything()
+        let cells: [Cell] = grid.addRow()
+        gridBub.addRow(with: cells)
+        grid.columns.filter({ $0.aggregate != .none }).forEach {
+            let footerTower: Tower = gridBub.aetherView.aetherExe.tower(key: $0.footerChain.key!)!
+            footerTower.buildStream()
+            footerTower.buildTask()
+        }
+        resizeEverything()
     }
     func addColumn() {
-//        let column: Column = grid.addColumn()
-//        gridBub.addColumn(with: column)
-//        resizeEverything()
+        let column: Column = grid.addColumn()
+        gridBub.addColumn(with: column)
+        resizeEverything()
     }
     
 // Sizing ==========================================================================================
     func resize() {
-//        guard needsResizing.count > 0 else { return }
-//        needsResizing.forEach { $0.resize() }
-//        needsResizing = []
-//
-//        guard columnNeedsResizing.count > 0 else { return }
-//        columnNeedsResizing.forEach { $0.resize() }
-//        columnNeedsResizing = []
-//
-//        guard leafNeedsResizing else { return }
-//        gridBub.gridLeaf.resize()
-//        leafNeedsResizing = false
-//        
-//        gridBub.render()
+        guard needsResizing.count > 0 else { return }
+        needsResizing.forEach { $0.resize() }
+        needsResizing = []
+
+        guard columnNeedsResizing.count > 0 else { return }
+        columnNeedsResizing.forEach { $0.resize() }
+        columnNeedsResizing = []
+
+        guard leafNeedsResizing else { return }
+        gridBub.gridLeaf.resize()
+        leafNeedsResizing = false
+        
+        gridBub.render()
     }
     func resizeEverything() {
         // This forces FooterCells to be generated, perhaps a more direct way is preferable
-//        gridBub.gridLeaf.gridView.reloadData()
-//        gridBub.gridLeaf.columns.forEach {
-//            $0.headerCell.setNeedsResize()
-//            $0.footerCell?.setNeedsResize()
-//            $0.gridCells.forEach { $0.setNeedsResize() }
-//        }
-//        resize()
-//        gridBub.render()
+        gridBub.gridLeaf.gridView.reloadData()
+        gridBub.gridLeaf.columns.forEach {
+            $0.headerCell.setNeedsResize()
+            $0.footerCell?.setNeedsResize()
+            $0.gridCells.forEach { $0.setNeedsResize() }
+        }
+        resize()
+        gridBub.render()
     }
     
 // Events ==========================================================================================
     func onAnchorCellTapped() { /*gridBub.morph()*/ }
     
     func nextGridCell(from gridCell: GridCell, release: Release) -> Editable? {
-//        switch release {
-//            case .focusTap, .administrative:
-//                return nil
-//            case .okEqualReturn:
-//                if gridBub.grid.equalMode == .close { return nil }
-//                return gridBub.gridLeaf.equalRelease(gridCell: gridCell)
-//            case .arrow(let arrow):
-//                return gridBub.gridLeaf.arrowRelease(gridCell: gridCell, arrow: arrow)
-//        }
-        nil
+        switch release {
+            case .focusTap, .administrative:
+                return nil
+            case .okEqualReturn:
+                if gridBub.grid.equalMode == .close { return nil }
+                return gridBub.gridLeaf.equalRelease(gridCell: gridCell)
+            case .arrow(let arrow):
+                return gridBub.gridLeaf.arrowRelease(gridCell: gridCell, arrow: arrow)
+        }
     }
     
 // ChainViewKeyDelegate ============================================================================
-//    func onArrowUp() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.up)) }
-//    func onArrowDown() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.down)) }
-//    func onArrowLeft() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.left)) }
-//    func onArrowRight() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.right)) }
-//    func onTab() { gridBub.rotateEndMode() }
+    func onArrowUp() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.up)) }
+    func onArrowDown() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.down)) }
+    func onArrowLeft() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.left)) }
+    func onArrowRight() { gridBub.gridLeaf.focusCell?.releaseFocus(.arrow(.right)) }
+    func onTab() { gridBub.rotateEndMode() }
 }
